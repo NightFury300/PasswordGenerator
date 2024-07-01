0b0c1164c03535fdabb6738658522e6a9c5d46b0
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-07-01 20:30:39 +0530

    Added different types of symbols for passwords logic as well as validating input

--- passwordGeneration.js
+++ passwordGeneration.js
@@ -1,12 +1,33 @@
-const button = document.querySelector("#generatePassButton");
-button.addEventListener('click',generatePassword);
 let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let numbers = '0123456789';
+let symbols = `!@#$%^&*()_{[}]|`;
+console.log(symbols)
 function generatePassword(length){
     let password = "";
-    for(let i = 0;i < 10;i++){
-        let idx = Math.floor(Math.random()*lowercase.length)
-        password += lowercase.charAt(idx);
+    for(let i = 0;i < length;i++){
+        password += generateRandomCharacter();
     }
-    console.log(password);
     return password;
+}
+
+function isAllowed(type){
+    return isCheckboxTrue(type);
+}
+
+function generateRandomCharacter(){
+    let type = -1;
+    while(!isAllowed(type)){
+        type = Math.floor(Math.random() * 4);
+    }
+    switch(type){
+        case 0:
+            return lowercase.charAt(Math.floor(Math.random()*lowercase.length));
+        case 1:
+            return uppercase.charAt(Math.floor(Math.random()*uppercase.length));
+        case 2:
+            return numbers.charAt(Math.floor(Math.random()*numbers.length));
+        case 3:
+            return symbols.charAt(Math.floor(Math.random()*symbols.length));
+    }
 }
--- updateUI.js
+++ updateUI.js
@@ -1,5 +1,43 @@
 const slider = document.querySelector("#slider");
 const sliderText = document.querySelector("#sliderVal");
+
+const field = document.querySelector("#passwordText");
+
+const lowercaseCheckbox = document.querySelector("#box0");
+const uppercaseCheckbox = document.querySelector("#box1");
+const numbersCheckbox = document.querySelector("#box2");
+const symbolsCheckbox = document.querySelector("#box3");
+
+
 slider.addEventListener('input',() => {
 sliderText.innerHTML = slider.value;
 })
+
+const button = document.querySelector("#generatePassButton");
+button.addEventListener('click',() => {
+    if(!checkForValidInput())
+        alert('You need to check atleast one checkbox!')
+    else
+        field.value = generatePassword(slider.value);
+});
+
+function checkForValidInput(){
+    //jwtx3k8mcvxtew
+    return  lowercaseCheckbox.checked ||
+            uppercaseCheckbox.checked ||
+            numbersCheckbox.checked ||
+            symbolsCheckbox.checked;
+}
+
+function isCheckboxTrue(id){
+switch(id){
+    case 0:
+        return lowercaseCheckbox.checked;
+    case 1:
+        return uppercaseCheckbox.checked;
+    case 2:
+        return numbersCheckbox.checked;
+    case 3:
+        return symbolsCheckbox.checked;
+}
+}
